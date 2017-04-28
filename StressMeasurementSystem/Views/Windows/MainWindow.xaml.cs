2cfb55f21289b93e96ac1a82428b0b1c94f6d19a
froth30 <theo.frohlich@gmail.com>
2017-04-27 23:23:51 -0400

    Move MainWindow to namespace "...Views.Windows"

--- StressMeasurementSystem/Views/Windows/MainWindow.xaml.cs
+++ StressMeasurementSystem/Views/Windows/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace StressMeasurementSystem
+namespace StressMeasurementSystem.Views.Windows
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
