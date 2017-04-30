1a741aaf9a0ae01ee1cc7325a7229daa9ad78221
froth30 <theo.frohlich@gmail.com>
2017-04-30 16:13:15 -0400

    Create default constructor

--- StressMeasurementSystem/ViewModels/PatientViewModel.cs
+++ StressMeasurementSystem/ViewModels/PatientViewModel.cs
@@ -1,7 +1,20 @@
-﻿namespace StressMeasurementSystem.ViewModels
+﻿using System.Collections.Generic;
+using StressMeasurementSystem.Models;
+
+namespace StressMeasurementSystem.ViewModels
 {
     public class PatientViewModel
     {
+        private Patient _patient;
 
+        public PatientViewModel()
+        {
+            _patient = new Patient(
+                new Patient.Name(), 0,
+                new Patient.Organization(),
+                new List<Patient.PhoneNumber>(),
+                new List<Patient.Email>()
+            );
+        }
     }
 }
