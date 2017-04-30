0e20a6d31cb10bf53e6b3e6f4216814a843368f8
froth30 <theo.frohlich@gmail.com>
2017-04-30 11:59:20 -0400

    Define Name struct

--- StressMeasurementSystem/Models/Patient.cs
+++ StressMeasurementSystem/Models/Patient.cs
@@ -2,6 +2,17 @@
 {
     public class Patient
     {
+        #region Structs
 
+        struct Name
+        {
+            public string Prefix { get; set; }
+            public string First { get; set; }
+            public string Middle { get; set; }
+            public string Last { get; set; }
+            public string Suffix { get; set; }
+        }
+
+        #endregion
     }
 }
