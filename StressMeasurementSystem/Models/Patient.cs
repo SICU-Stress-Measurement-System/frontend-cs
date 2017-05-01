6b13d45f8a103b82903973d490f8bf6a24bbdb6e
froth30 <theo.frohlich@gmail.com>
2017-05-01 14:22:22 -0400

    Reconfigure Patient as subclass of Contact

--- StressMeasurementSystem/Models/Patient.cs
+++ StressMeasurementSystem/Models/Patient.cs
@@ -1,82 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Net.Mail;
 
 namespace StressMeasurementSystem.Models
 {
-    public class Patient
+    public class Patient : Contact
     {
-        #region Structs
-
-        public struct NameInfo
+        public struct SocialSecurityNumber
         {
-            public string Prefix { get; set; }
-            public string First { get; set; }
-            public string Middle { get; set; }
-            public string Last { get; set; }
-            public string Suffix { get; set; }
+            public uint AreaNumber { get; set; }
+            public uint GroupNumber { get; set; }
+            public uint SerialNumber { get; set; }
 
             public override string ToString()
             {
-                return Prefix + " " + First + " " + Middle + " " + Last + ", " + Suffix;
+                return AreaNumber + "-" + GroupNumber + "-" + SerialNumber;
             }
         }
 
-        public struct OrganizationInfo
-        {
-            public string Company { get; set; }
-            public string JobTitle { get; set; }
-        }
-
-        public struct PhoneInfo
-        {
-            public enum Type { Mobile, Home, Work, Main, WorkFax, HomeFax, Pager, Other }
-
-            public string Number { get; set; }
-            public Type T { get; set; }
-        }
-
-        public struct EmailInfo
-        {
-            public enum Type { Home, Work, Other }
-
-            public MailAddress Address { get; set; }
-            public Type T { get; set; }
-        }
-
-        #endregion
-
-        #region Properties
-
-        public NameInfo? Name { get; set; }
-        public DateTime? DateOfBirth { get; set; }
-        public OrganizationInfo? Organization { get; set; }
-        public List<PhoneInfo> PhoneNumbers { get; set; }
-        public List<EmailInfo> EmailAddresses { get; set; }
-
-        #endregion
-
-        #region Constructors
+        public SocialSecurityNumber SSN { get; set; }
+        public DateTime DOB { get; set; }
 
         public Patient()
         {
-            Name = null;
-            DateOfBirth = null;
-            Organization = null;
-            PhoneNumbers = null;
-            EmailAddresses = null;
-        }
 
-        public Patient(NameInfo? nameInfo, DateTime? dateOfBirth, OrganizationInfo? organizationInfo,
-            List<PhoneInfo> phoneNumbers, List<EmailInfo> emailAddresses)
-        {
-            Name = nameInfo;
-            DateOfBirth = dateOfBirth;
-            Organization = organizationInfo;
-            PhoneNumbers = phoneNumbers;
-            EmailAddresses = emailAddresses;
         }
-
-        #endregion
     }
 }
