e61a2738407d4e2bda77469c43bf6822d272f9ab
froth30 <theo.frohlich@gmail.com>
2017-05-01 20:46:17 -0400

    Create test fixture skeleton

--- StressMeasurementSystem.Tests/Models/ContactTest.cs
+++ StressMeasurementSystem.Tests/Models/ContactTest.cs
@@ -5,6 +5,22 @@ namespace StressMeasurementSystem.Tests.Models
     [TestFixture]
     public class ContactTest
     {
+        [SetUp]
+        public void SetUp()
+        {
+            //TODO: Implement
+        }
 
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            //TODO: Implement
+        }
     }
 }
