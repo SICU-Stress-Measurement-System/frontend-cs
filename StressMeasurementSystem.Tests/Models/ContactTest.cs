2122c2c93b3e50aff7fba5a0beec8c9b2ce6befc
froth30 <theo.frohlich@gmail.com>
2017-05-07 15:30:38 -0400

    Implement SetUp method

--- StressMeasurementSystem.Tests/Models/ContactTest.cs
+++ StressMeasurementSystem.Tests/Models/ContactTest.cs
@@ -1,14 +1,53 @@
 ﻿using NUnit.Framework;
+using StressMeasurementSystem.Models;
+using System.Collections.Generic;
 
 namespace StressMeasurementSystem.Tests.Models
 {
     [TestFixture]
     public class ContactTest
     {
+        #region Fields
+
+        private Contact _contact;
+        private Contact.NameInfo _name;
+        private Contact.OrganizationInfo _organization;
+        private List<Contact.PhoneInfo> _phoneNumbers;
+        private List<Contact.EmailInfo> _emailAddresses;
+
+        #endregion
+
         [SetUp]
         public void SetUp()
         {
-            //TODO: Implement
+            _name = new Contact.NameInfo();
+            _name.First = "John";
+            _name.Last = "Smith";
+
+            _organization = new Contact.OrganizationInfo();
+            _organization.Company = "Smith Co.";
+            _organization.JobTitle = "CEO";
+
+            _phoneNumbers = new List<Contact.PhoneInfo>();
+
+            Contact.PhoneInfo phoneNumberWork = new Contact.PhoneInfo();
+            phoneNumberWork.Number = "(800) 123-4567";
+            phoneNumberWork.T = Contact.PhoneInfo.Type.Work;
+            _phoneNumbers.Add(phoneNumberWork);
+
+            Contact.PhoneInfo phoneNumberMobile = new Contact.PhoneInfo();
+            phoneNumberMobile.Number = "(123) 456-7890";
+            phoneNumberMobile.T = Contact.PhoneInfo.Type.Mobile;
+            _phoneNumbers.Add(phoneNumberMobile);
+
+            _emailAddresses = new List<Contact.EmailInfo>();
+
+            Contact.EmailInfo emailAddressWork = new Contact.EmailInfo();
+            emailAddressWork.Address = new System.Net.Mail.MailAddress("jsmith@smith.com");
+            emailAddressWork.T = Contact.EmailInfo.Type.Work;
+            _emailAddresses.Add(emailAddressWork);
+
+            _contact = new Contact(_name, _organization, _phoneNumbers, _emailAddresses);
         }
 
         [Test]
