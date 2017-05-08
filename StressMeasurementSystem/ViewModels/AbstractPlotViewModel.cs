31bdd02d0139c635441801709997f7e11fd9a460
froth30 <theo.frohlich@gmail.com>
2017-05-08 16:34:36 -0400

    Populate AbstractPlotViewModel with basic properties and default constructor

--- StressMeasurementSystem/ViewModels/AbstractPlotViewModel.cs
+++ StressMeasurementSystem/ViewModels/AbstractPlotViewModel.cs
@@ -1,7 +1,21 @@
-﻿namespace StressMeasurementSystem.ViewModels
+﻿using System.Collections.Generic;
+using OxyPlot;
+using OxyPlot.Wpf;
+
+namespace StressMeasurementSystem.ViewModels
 {
     public abstract class AbstractPlotViewModel
     {
+        protected PlotModel PlotModel { get; set; }
+        protected List<DataPointSeries> SeriesList { get; set; }
 
+        protected AbstractPlotViewModel()
+        {
+            PlotModel = new PlotModel
+            {
+                PlotAreaBackground = OxyColor.FromArgb(0, 0, 0, 0),
+                Title = "AbstractPlotView"
+            };
+        }
     }
 }
